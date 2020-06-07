3de39f676fb0aaada35bd059a29a182ac2349396
neno <neno@nenos-MacBook-Pro.local>
2020-06-06 22:19:02 -0300

    #3

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { AppLoading } from 'expo'
+import Routes from './src/routes'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+  return (
+    <>
+      <StatusBar barStyle="default" backgroundColor="transparent" translucent />
+      <Routes />
+    </>
+  );
+}
